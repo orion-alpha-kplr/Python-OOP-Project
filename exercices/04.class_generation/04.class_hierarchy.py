fcdcb20811133b1a14ab656dad1e5a13c07feb3e
orion-alpha-kplr <124896054+orion-alpha-kplr@users.noreply.github.com>
2023-03-08 11:28:55 +0000

    Pauline P

--- exercices/04.class_generation/04.class_hierarchy.py
+++ exercices/04.class_generation/04.class_hierarchy.py
@@ -19,9 +19,9 @@ def trimspaces(data):
 local_path = os.path.dirname(os.path.abspath(__file__))
 json_data = json.load(open(os.path.join(local_path, 'json_data.json'), "rb"))
 
-json_data = trimspaces(json_data)
+json_str = trimspaces(json_data)
 
-json_str = json.dumps(json_data)
+#json_str = json.dumps(json_data)
 
 json_data = (unidecode(json_str))
 json_dict = json.loads(json_data)
@@ -39,15 +39,13 @@ def generate_class_hierarchy(json_dict: dict, superclass_name: str = None, super
     # Initialisation de la chaîne de caractères contenant les définitions de classes
     class_defs = ""
     for class_name, class_attrs in json_dict.items():
-        class_def = exo02class_generation.generate_class_def(
-            class_name, class_attrs, superclass_name, superclass_args)
+        class_def = exo02class_generation.generate_class_def(class_name, class_attrs, superclass_name, superclass_args)
         class_defs += class_def
 
         if "subclasses" in class_attrs:
-            super_attr = (list(class_attrs.key()) + superclass_args)
+            super_attr = (list(class_attrs.keys()) + superclass_args)
             super_attr.remove("subclasses")
-            subclass_def = generate_class_hierarchy(
-                class_attrs["subclasses"], class_name, super_attr)
+            subclass_def = generate_class_hierarchy(class_attrs["subclasses"], class_name, super_attr)
             class_defs += subclass_def
 
     return class_defs
@@ -56,6 +54,10 @@ def generate_class_hierarchy(json_dict: dict, superclass_name: str = None, super
 def write_content(content, filename):
       with open(filename, "w", encoding='utf-8') as f:
            f.write(content)
+
+contenu = generate_class_hierarchy(json_dict)
+write_content(contenu, "product_classes.py")
+
 
 """ Itération sur les éléments du dictionnaire
        pour chaque nom de classe (class_name) et attribut de cette dernière (class_attrs) dans les éléments de  json_dict, faire:
